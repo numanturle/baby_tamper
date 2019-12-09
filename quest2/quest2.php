c4ae6dd16f5301421f089b93ca715287c29584d6
Numan Turle <numanturle@outlook.com.tr>
2019-12-09 11:42:40 +0300

    gitignore

--- quest2/quest2.php
+++ quest2/quest2.php
@@ -13,7 +13,7 @@ require_once "../db.php";
 
 
 
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
